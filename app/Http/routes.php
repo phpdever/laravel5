59302c37665279cf768aaa733c0461345b8769e5
phpdever <510438833@qq.com>
2017-05-02 09:03:05 +0800

    学习laravel框架

---
+++ app/Http/routes.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//多请求路由
+Route::match(['get','post'],'basic',function(){
+		return 'hello world';
+});
+
+//路由参数，过滤多个参数
+Route::get('user/{id}/{name?}',function($id,$name = 'phper'){
+	return $id;
+	return $name;
+})->where(['id'=>'[0-9]+','name'=>'[A-Za-z]+']);
+
+//路由别名
+Route::get('user/menber-center',['as'=>'center',function(){
+		return Route('center');
+}]);
+
+//路由群组
+Route::group(['prefix'=>'menber'],function(){
+	Route::get('info',function(){
+		return 1;
+	});
+});
+
+//路由视图
+Route::get('info',function(){
+	return view('info',['name'=>'phper','age'=>18]);
+});
+
+//关联控制器
+//Route::get('menber/info','MemberController@info');
+Route::get('menber/info',[
+	'uses'=>'MemberController@info'
+]);
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
