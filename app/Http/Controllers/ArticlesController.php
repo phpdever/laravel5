f08a20ecf4c9f5e91174073331ebe694e3162745
phpdever <510438833@qq.com>
2017-07-15 17:53:10 +0800

    添加注释

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -10,12 +10,14 @@ use App\Http\Requests;
 
 class ArticlesController extends Controller
 {
+	//循环文章列表
     public function index()
 	{
 			$articles = Article::all();
 			return view('articles/index',compact('articles'));
 	}
 	
+	//文章详情查看
 	public function show($id)
 	{
 			$articles = Article::find($id);
